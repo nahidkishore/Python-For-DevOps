576e313ed21bbeaca803b6e0d0fd27b4c416f710
nahidkishore <nahidkishore99@gmail.com>
2023-08-10 16:50:43 +0600

    python practice for devops

---
+++ cal.py
@@ -0,0 +1,5 @@
+num_1=float(input("Enter a number:"))
+num_2=float(input("Enter a another number:"))
+
+sum=num_1+num_2
+print(sum)
---
+++ test.py
@@ -0,0 +1,11 @@
+print("hello")
+print('test')
+print("this is python for devops master pro")
+name="niloy"
+age=13
+print(name)
+print(type(name))
+print(type(age))
+
+passions="devops pro"
+print( "My aim is", passions)
